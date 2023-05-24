eedb6c43409b9591c1d9338131bc91675bf11140
ChiaraS97 <chiara.sucato97@gmail.com>
2023-05-24 16:01:07 +0200

    create migration

---
+++ database/migrations/2023_05_24_132830_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 20);
+            $table->string('departure_station', 20);
+            $table->string('arrival_station', 20);
+            $table->timeTz('departure_time', $precision = 0);
+            $table->timeTz('arrival_time', $precision = 0);
+            $table->smallInteger('code')->unsigned();
+            $table->tinyInteger('coaches')->unsigned();
+            $table->boolean('on_time')->default(1);
+            $table->boolean('canceled')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
